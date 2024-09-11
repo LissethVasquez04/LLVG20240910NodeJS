52ff7d908c9b3f2ac4b2f46c7ce74befaadc178f
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-09-10 21:29:34 -0600

    Router

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const ProductLLVGController = require('../controllers/ProductLLVGController'); // Asegúrate de que la ruta sea correcta
 
+// Definir las rutas
+router.get('/', ProductLLVGController.getAllProducts); // Asegúrate de que getAllProducts esté definido en el controlador
+router.get('/:id', ProductLLVGController.getProductById); // Asegúrate de que getProductById esté definido en el controlador
+router.post('/', ProductLLVGController.createProduct); // Asegúrate de que createProduct esté definido en el controlador
+router.put('/:id', ProductLLVGController.updateProduct); // Asegúrate de que updateProduct esté definido en el controlador
+router.delete('/:id', ProductLLVGController.deleteProduct); // Asegúrate de que deleteProduct esté definido en el controlador
+
+module.exports = router;
